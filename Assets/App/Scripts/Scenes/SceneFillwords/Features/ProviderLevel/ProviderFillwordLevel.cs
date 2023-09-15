1b0433c54cda2b829c381f790419af2015d4acdd
mk1quant <mark.quant11@gmail.com>
2023-09-15 04:07:58 +0300

    Updated Fillwords Logic

--- Assets/App/Scripts/Scenes/SceneFillwords/Features/ProviderLevel/ProviderFillwordLevel.cs
+++ Assets/App/Scripts/Scenes/SceneFillwords/Features/ProviderLevel/ProviderFillwordLevel.cs
@@ -4,81 +4,52 @@ using App.Scripts.Scenes.SceneFillwords.Features.FillwordModels;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
 {
     public class ProviderFillwordLevel : IProviderFillwordLevel
     {
-        //public GridFillWords LoadModel(int index)
-        //{
-        //    var _levelsData ??= CacheLevelData();
+        private const string PATH_WORDS_LIST = "Fillwords/words_list";
+        private const string PATH_PACK = "Fillwords/pack_0";
 
-        //    int levelIndex = index - 1;
+        private string[] wordsList;
+        private string[] pack;
 
-        //    for(int i = levelIndex; i < _levelData.Lenght; i++)
-        //    {
-        //        var level = LoadLevel(i);
+        public ProviderFillwordLevel()
+        {
+            wordsList = Resources.Load<TextAsset>(PATH_WORDS_LIST).text.Split('\r')
+            .Select(line => line.Trim()).ToArray();
 
-        //        if (level != null)
-        //            return level;
-        //    }
-
-        //    throw new Exception();
-        //}
-
-        //private char[] CacheLevelData()
-        //{
-        //    return null;
-        //}
-
-        //private void LoadLevel(int i)
-        //{
-
-        //}
+            pack = Resources.Load<TextAsset>(PATH_PACK).text.Split('\r')
+            .Select(line => line.Trim()).ToArray();
+        }
 
         public GridFillWords LoadModel(int index)
         {
             int levelIndex = index - 1;
 
-            string[] wordsList = Resources.Load<TextAsset>("Fillwords/words_list").text.Split('\r');
-            string[] pack = Resources.Load<TextAsset>("Fillwords/pack_0").text.Split('\r');
-
-            for (int i = 0; i < wordsList.Length; i++)
+            for (int i = levelIndex; i < pack.Length; i++)
             {
-                wordsList[i] = wordsList[i].Trim();
-            }
-            for (int i = 0; i < pack.Length; i++)
-            {
-                pack[i] = pack[i].Trim();
-            }
-
-
-            for(int i = levelIndex; i < pack.Length; i++)
-            {
-                var level = LoadLevel(i, wordsList, pack);
+                var level = LoadLevel(i);
 
                 if (level != null)
                     return level;
             }
 
-            throw new Exception();
-            return null;
-
-            ////напиши реализацию не меняя сигнатуру функции
-            //throw new NotImplementedException();
+            throw new Exception("No valid level found");
         }
 
-        private GridFillWords LoadLevel(int index, string[] wordsList, string[] pack)
+        private GridFillWords LoadLevel(int index)
         {
             try
             {
-                int size;
-
                 string[] levelLogic = pack[index].Split(' ');
                 int wordsCount = levelLogic.Length / 2;
 
-                string[] words = new string[wordsCount]; // массив со словами
-                string[] letterArrang = new string[wordsCount]; // массив с расстановками слов
+                string[] words = new string[wordsCount];
+                string[] letterArrang = new string[wordsCount];
 
                 for (int i = 0, j = 0; i < levelLogic.Length; i = i + 2, j++)
                 {
@@ -91,7 +62,7 @@ namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
                 //foreach (var w in letterArrang)
                 //    Debug.Log("Letter arrang: " + w + ", " + w.Length);
 
-                size = GetSize(words);
+                int size = GetSize(words);
 
                 char[] fillWord = FillWord(size, letterArrang, words);
 
@@ -99,6 +70,7 @@ namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
             }
             catch (Exception e)
             {
+                Debug.LogException(e);
                 //throw new Exception();
                 return null;
             }
@@ -106,19 +78,17 @@ namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
 
         private int GetSize(string[] words)
         {
-            int wordsLenght = 0;
-            foreach (var word in words)
-                wordsLenght += word.Length;
-            
-            int size = (int)Math.Sqrt(wordsLenght);
+            int wordsLength = words.Sum(word => word.Length);
+            int size = (int)Math.Ceiling(Math.Sqrt(wordsLength));
 
-            if ((size * size) == wordsLenght)
+            if (size * size == wordsLength)
+            {
                 return size;
+            }
             else
             {
-                throw new Exception("Wrong size");
-                return 0;
-            }     
+                throw new Exception("No valid size");
+            }   
         }
 
         private char[] FillWord(int size, string[] letterArrang, string[] words)
@@ -127,13 +97,23 @@ namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
 
             for (int i = 0; i < letterArrang.Length; i++)
             {
-                string[] pos = letterArrang[i].Split(";");
+                string[] positions = letterArrang[i].Split(";");
+
+                if(positions.Distinct().Count() != positions.Length)
+                {
+                    throw new Exception("Found repetitive position");
+                }
+
+                if (positions.Any(position => Convert.ToInt32(position) < 0 || Convert.ToInt32(position) >= size * size))
+                {
+                    throw new ArgumentException("Invalid position");
+                }
+
                 string word = words[i];
 
                 for (int j = 0; j < word.Length; j++)
                 {
-                    int position = Convert.ToInt32(pos[j]);
-
+                    int position = Convert.ToInt32(positions[j]);
                     fillWord[position] = word[j];
                 }
             }
@@ -151,7 +131,6 @@ namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
                 {
                     gridFillWords.Set(i, j, new CharGridModel(letters[letterIndex]));
                 }
-
             }
 
             return gridFillWords;
